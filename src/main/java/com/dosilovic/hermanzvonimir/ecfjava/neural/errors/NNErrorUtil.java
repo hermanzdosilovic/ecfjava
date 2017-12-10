b7badf0fb67a63972fb2a997dba8a4041a60d4f4
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-12-10 22:52:02 +0100

    Removed some unused error functions and utilities

---
+++ src/main/java/com/dosilovic/hermanzvonimir/ecfjava/neural/errors/NNErrorUtil.java
@@ -0,0 +1,24 @@
+package com.dosilovic.hermanzvonimir.ecfjava.neural.errors;
+
+import com.dosilovic.hermanzvonimir.ecfjava.neural.INeuralNetwork;
+import com.dosilovic.hermanzvonimir.ecfjava.util.DatasetEntry;
+
+public final class NNErrorUtil {
+
+    public static double meanSquaredError(INeuralNetwork neuralNetwork, DatasetEntry... dataset) {
+        double   error = 0;
+        double[] networkOutput;
+        double[] actualOutput;
+
+        for (DatasetEntry datasetEntry : dataset) {
+            networkOutput = neuralNetwork.forward(datasetEntry.getInput());
+            actualOutput = datasetEntry.getOutput();
+
+            for (int i = 0; i < actualOutput.length; i++) {
+                error += Math.pow(networkOutput[i] - actualOutput[i], 2);
+            }
+        }
+
+        return error / dataset.length;
+    }
+}
