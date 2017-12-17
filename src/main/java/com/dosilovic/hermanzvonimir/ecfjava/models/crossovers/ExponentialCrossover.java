56b5e7016951098a4640c870bd442c1782032318
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-12-17 15:20:33 +0100

    Implemented exponential crossover

--- src/main/java/com/dosilovic/hermanzvonimir/ecfjava/models/crossovers/ExponentialCrossover.java
+++ src/main/java/com/dosilovic/hermanzvonimir/ecfjava/models/crossovers/ExponentialCrossover.java
@@ -7,21 +7,22 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Random;
 
-public class UniformCrossover<T extends IVector> implements ICrossover<T> {
+public class ExponentialCrossover<T extends IVector> implements ICrossover<T> {
 
     private double crossoverProbability = 0.5;
 
     private static final Random RAND = new Random();
 
-    public UniformCrossover(double crossoverProbability) {
+    public ExponentialCrossover(double crossoverProbability) {
         this.crossoverProbability = crossoverProbability;
     }
 
-    public UniformCrossover() {}
+    public ExponentialCrossover() {}
 
-    @SuppressWarnings("unchecked")
     @Override
-    public Collection<Solution<T>> cross(Solution<T> firstParent, Solution<T> secondParent) {
+    public Collection<Solution<T>> cross(
+        Solution<T> firstParent, Solution<T> secondParent
+    ) {
         T firstParentRepresentative  = firstParent.getRepresentative();
         T secondParentRepresentative = secondParent.getRepresentative();
 
@@ -32,10 +33,16 @@ public class UniformCrossover<T extends IVector> implements ICrossover<T> {
         children.add(new Solution<>(firstChild));
         children.add(new Solution<>(secondChild));
 
-        for (int i = 0; i < firstParentRepresentative.getSize(); i++) {
+        int start = RAND.nextInt(firstParentRepresentative.getSize());
+        firstChild.setValue(start, secondParentRepresentative.getValue(start));
+        secondChild.setValue(start, firstParentRepresentative.getValue(start));
+
+        for (int i = start + 1; i < firstParentRepresentative.getSize(); i++) {
             if (RAND.nextDouble() <= crossoverProbability) {
                 firstChild.setValue(i, secondParentRepresentative.getValue(i));
                 secondChild.setValue(i, firstParentRepresentative.getValue(i));
+            } else {
+                break;
             }
         }
 
